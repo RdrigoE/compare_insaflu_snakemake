7180cd8228fcac32ebc33f49c99ec1cff74adad4
Rodrigo Eusébio <rodrigojeusebio@gmail.com>
2023-09-05 11:04:06 +0100

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # compare_insaflu_snakemake
+
+## Install and activate compare_insaflu environment 
+```
+mamba env create --name compare_insaflu --file compare_insaflu.yaml
+conda activate compare_insaflu
+```
+## To set up the directory
+
+Copy the main_results from INSaFLU and from INSaFLU-Snakemake.
+
+Make sure that the fasta identifiers are exactly the same.
+The coverage files as well.
+
+## Compare multisegmented genomes
+
+`bash compare_all.sh 8` 
+
+This is an example for Influenza
+
+## Compare one segmented genomes
+
+`bash compare_all.sh 1` 
+
+This is an example for SARS-CoV-2 and Monkeypox.
+
+
+
--- compare_all.sh
+++ compare_all.sh
@@ -1,13 +1,20 @@
-source insaflu
+#!/bin/bash
+
+number_of_locus="$1"
 echo "Comparing AllConsensus"
 python compare_consensus.py --f1 ./snakemake/AllConsensus_no_ref.fasta --f2 ./insaflu/AllConsensus.fasta --output all_consensus_output.csv -v
-# echo "Comparing Alignment_nt_All"
-# python compare_consensus.py --f1 ./snakemake/Alignment_nt_All.fasta --f2 ./insaflu/Alignment_nt_All.fasta --output alignment_output.csv -v
-echo "Comparing Coverage"
-python compare_coverage.py --f1 snakemake/coverage.csv --f2 insaflu/coverage.tsv --n_locus 8 --output coverage_output.csv -v
-echo "Comparing Validated Variants"
-python compare_snps.py --f1 snakemake/validated_variants.csv --f2 insaflu/validated_variants.tsv --output validated_output.csv -v
-echo "Comparing Minor Variants"
-python compare_snps.py --f1 snakemake/validated_minor_iSNVs.csv --f2 insaflu/validated_minor_iSNVs.tsv --output minor_output.csv -v
 
+if [[ "$number_of_locus" == "1" ]]; then
+    echo "Comparing Alignment_nt_All"
+    python compare_consensus.py --f1 ./snakemake/Alignment_nt_All.fasta --f2 ./insaflu/Alignment_nt_All.fasta --output alignment_output.csv -v
+fi
+
+echo "Comparing Coverage"
+python compare_coverage.py --f1 ./snakemake/coverage.csv --f2 ./insaflu/coverage.tsv --n_locus "$number_of_locus" --output coverage_output.csv -v
+
+echo "Comparing Validated Variants"
+python compare_snps.py --f1 ./snakemake/validated_variants.csv --f2 ./insaflu/validated_variants.tsv --output validated_output.csv -v
+
+echo "Comparing Minor Variants"
+python compare_snps.py --f1 ./snakemake/validated_minor_iSNVs.csv --f2 ./insaflu/validated_minor_iSNVs.tsv --output minor_output.csv -v
 
